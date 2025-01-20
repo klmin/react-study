05235376b8eb31b06386a9aff6ee8cf2f997ff84
klmin <haemmas6394@gmail.com>
2025-01-20 17:27:47 +0900

    test : component

--- src/circle.js
+++
@@ -1,9 +0,0 @@
-export const PI = 3.141592;
-
-export function area(radius) {
-  return PI * radius * radius;
-}
-
-export function circumference(radius) {
-  return 2 * PI * radius;
-}
--- src/index.js
+++ src/index.js
@@ -2,33 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {PI,area,circumference } from './circle';
-import * as circle from "./circle";
-import aa from "./test";
-import lodash from "lodash"; 
-
-const arr = [1, 1, 1, 2, 2, 1, 1, 4, 4, 3, 2];
-const uniqueArr = lodash.uniqBy(arr); 
-
-console.log(uniqueArr);
- 
-console.log(`PI : ${PI}`)
-console.log(`area : ${area(10)}`)
-console.log(`circumference : ${circumference(10)}`)
-console.log(`circle.PI : ${circle.PI}`)
-console.log(`circle.area : ${circle.area(10)}`)
-console.log(`circle.circumference : ${circle.circumference(10)}`)
-console.log(`PI1 : ${aa}`)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
+
+
