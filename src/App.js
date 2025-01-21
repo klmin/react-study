94b863901181ab4b583c9dee149fce48baadc135
klmin <haemmas6394@gmail.com>
2025-01-21 11:11:54 +0900

    test: Add Props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-import Header from './component/Header'
-import Body from './component/Body'
-import Footer from './component/Footer'
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer'
 
 const App = () => {
+  const props = {
+    name : "testName",
+    location : "서울",
+    //favorList : ["파스타", "빵", "떡볶이"],
+  }
+
   return (
       <div className="App">
         <Header />
-        <Body />
+        <Body {...props} />
         <Footer />
       </div>
   );
