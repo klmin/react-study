fb9db996fe9234893df9c1caa22de7d051fa37d2
klmin <haemmas6394@gmail.com>
2025-01-22 15:49:32 +0900

    feat: Add examples for hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,32 +4,25 @@ import { useRef, useState, useEffect } from "react";
 const Body = () => {
 
     const [text, setText] = useState("");
-    const [text1, setText1] = useState("");
     const textRef = useRef();
 
     const handleOnChange = (e) => {
-        console.log(`text change value : ${e.target.value}`)
         setText(e.target.value);
     }
-    const handleOnChange1 = (e) => {
-        console.log(`text1 change value : ${e.target.value}`)
-        setText1(e.target.value);
-    }
-
+   
     const handleOnClick = () => {
+        if(text.length < 5){
+            textRef.current.focus();
+            return;
+        }
         alert(text);
-        textRef.current.value = "";
+        setText("")
     }   
-
-    useEffect(() => {
-        console.log(`update text : ${text}`);
-    }, [text]);
 
     return (
         <div>
             <input ref={textRef} value={text} onChange={handleOnChange}/>
             <button onClick={handleOnClick}>작성 완료</button>
-            <input value={text1} onChange={handleOnChange1}/>
         </div>
     );
 }
